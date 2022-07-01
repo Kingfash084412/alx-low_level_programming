08a0115f5095e47dc4c661fc1b35bfa1e9b98735
Fashina <fashina.segun25@gmail.com>
2022-07-01 15:39:58 -0700

    advance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - prints a standard error
- *
- * Return: Always 1 (Success)
+ * main - Entry point
+ * Return: 1.
  */
+
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
