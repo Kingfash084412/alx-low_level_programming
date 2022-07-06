df567d45552b4d98de01159392161a4e12303506
Fashina <fashina.segun25@gmail.com>
2022-07-05 17:25:16 -0700

    functions nested loops task 0 fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - function to print
+ *
+ * Return: 0
  */
-
 int main(void)
 {
-char *sh = "main";
-while (*sh)
-{
-_putchar
-sh++;
-}
-_putchar('\n');
-return (0);
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+	return (0);
+}
