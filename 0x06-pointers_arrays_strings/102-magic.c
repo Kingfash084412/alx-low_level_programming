17ddea61f137ce2a61886cd5a8ba8801d382ca1e
Fashina <fashina.segun25@gmail.com>
2022-07-15 00:09:24 -0700

    0x06-pointers_arrays_strings task 12

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,89 @@
+#include "main.h"
+
 #include <stdio.h>
 
-int main(void);
-{
-	int n;
-	int a[5];
-	int *p;
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	p[5] = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+
+/**
+ *
+ *  * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *
+ *   *                the byte position, then showing the hex content,
+ *
+ *    *                then displaying printable charcaters.
+ *
+ *     * @b: The buffer to be printed.
+ *
+ *      * @size: The number of bytes to be printed from the buffer.
+ *
+ *       */
+
+
+
+void print_buffer(char *b, int size)
+
+{
+
+		int byte, index;
+
+
+
+			for (byte = 0; byte < size; byte += 10)
+
+					{
+
+								printf("%08x: ", byte);
+
+
+
+										for (index = 0; index < 10; index++)
+
+													{
+
+																	if ((index + byte) >= size)
+
+																						printf("  ");
+
+																				else
+
+																									printf("%02x", *(b + index + byte));
+
+																							if ((index % 2) != 0 && index != 0)
+
+																												printf(" ");
+
+																									}
+
+												for (index = 0; index < 10; index++)
+
+															{
+
+																			if ((index + byte) >= size)
+
+																								break;
+
+																						else if (*(b + index + byte) >= 31 &&
+
+																												 *(b + index + byte) <= 126)
+
+																											printf("%c", *(b + index + byte));
+
+																									else
+
+																														printf(".");
+
+																											}
+
+														if (byte >= size)
+
+																		continue;
+
+																printf("\n");
+
+																	}
+
+				if (size <= 0)
+
+							printf("\n");
+
 }
