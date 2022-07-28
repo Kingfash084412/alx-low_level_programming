6dbfbd246dff3053c3bc4d3389ff05fb20e85598
Fashina <fashina.segun25@gmail.com>
2022-07-28 10:30:31 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Header file containing prototypes for all functions
- *       used in the 0x0C-more_malloc_free directory.
- */
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
