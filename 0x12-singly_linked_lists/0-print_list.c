ca42bb531c29cc32f22bcac21f84f5f5547e8925
Fashina Segun <fashina.segun25.com>
2022-08-11 17:24:27 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,27 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list
+ * print_list - prints all the elements of a list
+ *
+ * @h:pointer to the list
+ *
+ * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+const list_t *cursor = h;
+size_t count = 0;
 
-	nelem = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nelem++;
-	}
-	return (nelem);
+while (cursor != NULL)
+{
+if (cursor->str != NULL)
+printf("[%d] %s\n", cursor->len, cursor->str);
+else
+printf("[0] (nil)\n");
+count += 1;
+cursor = cursor->next;
 }
 
-
+return (count);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,1 +1,1 @@
-0x12-singly_linked_lists
+A linked list is a linear collection of data elements whose order is not given by their physical placement in memory. Instead, each element points to the next. It is a data structure consisting of a collection of nodes which together represent a sequence. In its most basic form, each node contains: data, and a reference (in other words, a link) to the next node in the sequence.
