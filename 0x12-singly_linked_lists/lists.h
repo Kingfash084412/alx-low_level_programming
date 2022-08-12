82588f209a1ecebbf82bcb6d81dd7b2d8a0100a0
Fashina Segun <fashina.segun25.com>
2022-08-11 18:37:41 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 /**
  * File: lists.h
@@ -35,5 +36,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void before_main(void)__attribute__((constructor));
 
-#endif /* LISTS_H *i/
+#endif
