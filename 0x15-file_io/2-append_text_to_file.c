db4320601efa45bb59f422ce49061559433e2d90
Fashina Segun <fashina.segun25@gmail.com>
2022-08-22 13:53:05 +0000

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * append_text_to_file - function to append text at the end of the file
  * @filename: pointer to the file
@@ -8,23 +9,33 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int reader, output, count;
+	int file, wr, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	reader = open(filename, O_WRONLY | O_APPEND);
-
-	if (reader == -1)
+	file = open(filename, O_RDWR | O_APPEND);
+	if (file == -1)
 		return (-1);
+
+
+
+	while (text_content[i])
+		i++;
+
 	if (text_content == NULL)
+	{
+		close(file);
 		return (1);
+	}
+	else
+	{
+		wr = write(file, text_content, i);
+	}
 
-	for (; text_content[count] != '\0'; count++)
-		;
-	output = write(reader, text_content, count);
-	if (output == -1)
+	if (wr == -1)
 		return (-1);
-	close(reader);
+
+	close(file);
 	return (1);
 }
